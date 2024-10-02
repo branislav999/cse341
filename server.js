acb03ba0fd052ef0ed8f676d985957fd8967805b
branislav999 <bbogosavac1999@gmail.com>
2024-10-01 22:25:06 -0600

    idegas

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const contactsRouter = require('./routes/contacts');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 5000; 
 
 app.use(express.json());
 
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/contacts', contactsRouter);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Contacts API');
+    res.send('Contacts');
 });
 
 app.listen(PORT, () => {
